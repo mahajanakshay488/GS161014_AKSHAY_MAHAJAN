57228c1166ebcf02f5b14df9fda975822c720346
akshay <mahajanakshay488@gmail.com>
2025-03-07 14:35:47 +0530

    feat: sidenave updated with links and icons

--- src/components/utils/Sidenav.tsx
+++ src/components/utils/Sidenav.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { NavLink } from 'react-router';
+import { navLinks } from '../../utilities/constants';
 
 export default function Sidenav() {
   return (
-    <nav className='h-full bg-white w-[200px] fixed'>
-        <p>nav links</p>
+    <nav className='h-full pt-4 bg-white w-[200px] fixed'>
+
+      {
+        navLinks.map((element) => (
+          <NavLink 
+          to={element.path}
+          className={({isActive})=>(`flex justify-start items-center p-2 py-3 ${isActive && 'bg-gray-200'}`)}
+        >
+
+          <span className="material-symbols-outlined text-2xl">
+            {element.icon}
+          </span>
+
+          <h4 className='text-lg font-medium ml-2'>{element.title}</h4>
+
+        </NavLink>
+        ))
+      } 
     </nav>
   )
 }
---
+++ src/utilities/constants.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    path: '/',
+    icon: 'store',
+    title: 'Store'
+  },
+  {
+    path: '/sku',
+    icon: 'category',
+    title: 'SKU'
+  },
+  {
+    path: '/planning',
+    icon: 'finance',
+    title: 'Planning'
+  },
+  {
+    path: '/charts',
+    icon: 'insert_chart',
+    title: 'Charts'
+  }
+]
